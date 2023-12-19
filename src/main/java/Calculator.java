809b7e504124396d86c9f0ff72d7f6104480b2c1
Kirgly1 <kirves03@yandex.ru>
2023-12-20 01:47:08 +0300

    Test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,32 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a-b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b ==0){
+            System.out.println("Не дели на 0");
+        }
+        return a / b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+    public int solver(int operation, int a, int b){
+        switch (operation) {
+            case 1:
+                return add(a, b);
+            case 2:
+                return dif(a, b);
+            case 3:
+                return div(a, b);
+            case 4:
+                return times(a, b);
+            default:
+                System.out.println("Неправильно, попробуйте еще раз");
+                return 0; // or handle the error in a different way
+        }
     }
 }
